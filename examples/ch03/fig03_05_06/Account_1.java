aa82dca2609d815baaea0d1a322cc0f4f5f61878
ivcerveira <ivaldocerveira@outlook.com>
2025-09-09 19:35:50 -0300

    Add TipCalculator

--- examples/ch03/fig03_05_06/Account_1.java
+++ examples/ch03/fig03_05_06/Account_1.java
@@ -2,26 +2,20 @@ package ch03.fig03_05_06;
 // Fig. 3.5: Account_1.java
 // Account_1 class with a constructor that initializes the name.
 
+@SuppressWarnings("ClassCanBeRecord")
 public class Account_1
 {
-   private String name; // instance variable
+   private final String name; // instance variable
 
    // constructor initializes name with parameter name
    public Account_1(String name) // constructor name is class name
    {                                                               
       this.name = name;
-   }                                            
+   }
 
-   // method to set the name
-   public void setName(String name)
-   {
-      this.name = name; 
-   } 
-
-   // method to retrieve the name
+    // method to retrieve the name
    public String getName()
    {
       return name; 
    } 
 } // end class Account_1
-
---
+++ examples/ch25/tipCalculator/src/TipCalculator.java
@@ -0,0 +1,31 @@
+package ch25.tipCalculator.src;
+// TipCalculator.java
+// Main application class that loads and displays the Tip Calculator's GUI.
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class TipCalculator extends Application 
+{
+   @Override
+   public void start(Stage stage) throws Exception 
+   {
+      Parent root = 
+         FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TipCalculator.fxml")));
+
+      Scene scene = new Scene(root); // attach scene graph to scene
+      stage.setTitle("Tip Calculator"); // displayed in window's title bar
+      stage.setScene(scene); // attach scene to stage
+      stage.show(); // display the stage
+   }
+
+   public static void main(String[] args) 
+   {
+      // create a TipCalculator object and call its start method
+      launch(args); 
+   }
+}
